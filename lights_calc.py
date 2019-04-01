f10fdf22dbdf28ad52c5fda8e7ef882c83ecfccf
U-plus2620 <39313494+U-plus2620@users.noreply.github.com>
2019-04-01 20:25:29 +0300

    Update lights_calc.py

--- lights_calc.py
+++ lights_calc.py
@@ -1,42 +1,76 @@
 #!/usr/bin/python
 import math
 import sys
+import getopt
 
 
 
-print("usage {} lux a b lum".format(sys.argv[0]))
-
-
-global lx
-lx=int(sys.argv[1])
-global lx_result_roundDown
-global lx_result_roundUp
+global Fu
 global num_of_fixtures
 global k
-k=0.7
-global Fu
-Fu=50
-global a
-a=float(sys.argv[2])
-global b
-b=float(sys.argv[3])
+global lx_result_roundDown
+global lx_result_roundUp
+global lux
+global room_size
 global lum
-lum=int(sys.argv[4])
 global const
+
+
+
+k=0.7
+Fu=50
 const=100
 
-print(" lx => {}\n a => {}\n b => {}\n lum => {}".format(lx,a,b,lum))
-def light_calc(lx,a,b,lum):
-	equation=(lx*a*b*const)/(k*Fu*lum)
+def usage():
+	print("usage {} --lux --area --lum [-k,--Fu]\n ".format(sys.argv[0]))
+	print("--lux  => is the necessary lux for given area.")
+	print("--area => is the size of the area given in square meters.")
+	print("--lum  => is the lumen's given off by a given light fixture.")
+	print("-k     => is the dust acumalation coefficient (that acumalates on the fixture)\n\tThis value is hard coded to an average value but could optionaly be changed using the -k argument")
+	print("--Fu   => is the coefficient of reflection that is reflected off by the walls")
+	exit()
+
+def light_calc(lux,area,Fu,k,lum):
+	print(" lux => {}\n area => {}\n lum => {}\n k => {}\n Fu => {}".format(lux,area,lum,k,Fu))
+	equation=(lux*area*const)/(k*Fu*lum)
 	result=equation
-	num_of_fixtures_down=math.floor(equation)
+	num_of_fixtures_down=math.floor(result)
 	num_of_fixtures_up=math.ceil(result)
-	lx_result_roundDown=(num_of_fixtures_down*k*Fu*lum)/(a*b*const)
-	lx_result_roundUp=(num_of_fixtures_up*k*Fu*lum)/(a*b*const)
-	print("result {}\n actual number of fixtures:\n  Rounded down {},\n  Rounded up   {}\n actual lux level:\n  Rounded down {}\n  Rounded up   {}".format(result,num_of_fixtures_down,num_of_fixtures_up,lx_result_roundDown,lx_result_roundUp))
+	lx_result_roundDown=(num_of_fixtures_down*k*Fu*lum)/(area*const)
+	lx_result_roundUp=(num_of_fixtures_up*k*Fu*lum)/(area*const)
+	print("\nresult {}\n \n\tactual number of fixtures:\n  \t\tRounded down {},\n  \t\tRounded up   {}\n \tactual lux level:\n  \t\tRounded down {}\n  \t\tRounded up   {}".format(result,num_of_fixtures_down,num_of_fixtures_up,lx_result_roundDown,lx_result_roundUp))
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:],"k:",["help","lux=","lum=","Fu=","area="])
+except getopt.GetoptError as err:
+    print(str(err))
+    usage()
+if not sys.argv[1:]:
+	usage()
+
+for o,a in opts:
+    if o in ('--help'):
+        usage()
+    elif o in ('-k'):
+       k=int(a)
+    elif o in ('--lux'):
+        lux=int(a)
+    elif o in ('--lum'):
+	lum=int(a)
+    elif o in ('--area'):
+	area=int(a)
+    elif o in ('--Fu'):
+	Fu=int(a)
+
+
+
+
+
+
 	#power_up  = 
 	#amp_up    =
 	#power_down=
 	#amp_down  =	
 
-light_calc(lx,a,b,lum)
+light_calc(lux,area,lum,Fu,k)
+
